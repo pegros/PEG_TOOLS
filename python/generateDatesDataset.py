483bae3f57fdc6915cf8fd0fdb42f902702cb22f
Pierre-Emmanuel Gros <pgros@pgros-ltm9.internal.salesforce.com>
2022-03-07 18:34:08 +0100

    date dataset extension

--- python/generateDatesDataset.py
+++ python/generateDatesDataset.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 from datetime import date, timedelta
 
+weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 sdate = date(1970, 1, 1)     # start date
 edate = date(2069, 12, 31)   # end date
 delta = edate - sdate        # as timedelta
 
-print("Date,Count")
+print("Date,Count,Week,Day")
 for i in range(delta.days + 1):
     day = sdate + timedelta(days=i)
     # print(day & ",1")
-    print str(day) + ",1"
+    print str(day) + ",1," + str(day.isocalendar()[1]) + "," + weekDays[(day.weekday())]
